efa332a84d2481f1e3584fbeecaf5a754779875b
raul-vital <raul.rsv@hotmail.com>
2024-09-30 18:54:26 -0700

    add imports, establish connection to server, add homepage get route

---
+++ server.js
@@ -0,0 +1,29 @@
+//********* IMPORTS **********/
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+//********* IMPORTS **********/
+const port = process.env.PORT ? process.env : '3000'
+
+//********* ROUTES **********/
+app.get('/', (req, res) => {
+    res.send("Welcome to Reci-Pies!")
+})
+
+
+
+
+
+
+
+
+
+
+//********* LISTENER **********/
+app.listen(port, () => {
+    console.log(`Listening in P(ie)ORT.... ${port}`)
+})
