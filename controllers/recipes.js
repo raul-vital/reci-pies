4e1d74fcb60693943d382c27bd8bbaa777f3bd3f
raul-vital <raul.rsv@hotmail.com>
2024-09-30 22:20:54 -0700

    add /new get route, add new.ejs file with form for adding new rrecipe, method POST, add link to /new at recipes/index.ejs

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,10 +3,14 @@ const router = express.Router()
 
 const User = require('../models/user')
 
+//********* ROUTES **********/
 router.get('/', async (req, res)=>{
     const user = await User.findById(req.session.user._id)
     res.render('recipes/index.ejs', {recipes: user.recipes})
 })
 
+router.get('/new', (req,res)=>{
+    res.render('./new.ejs')
+})
 
 module.exports = router
