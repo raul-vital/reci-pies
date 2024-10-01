20b1077dc44bdd6e54ef89494c951d3a4e524faf
raul-vital <raul.rsv@hotmail.com>
2024-10-01 12:51:02 -0700

    add try,catch into get router, add post router and fix ejs issue at new.ejs

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,13 +4,30 @@ const router = express.Router()
 const User = require('../models/user')
 
 //********* ROUTES **********/
-router.get('/', async (req, res)=>{
+router.get('/', async (req,res)=>{
+    try{
     const user = await User.findById(req.session.user._id)
     res.render('recipes/index.ejs', {recipes: user.recipes})
+    }catch(error){
+        console.log(error)
+        res.redirect('/')
+    }
 })
 
 router.get('/new', (req,res)=>{
     res.render('./new.ejs')
 })
 
+router.post('/', async (req,res)=>{
+   try{
+    const user = await User.findById(req.session.user._id)
+    user.recipes.push(req.body)
+    await user.save()
+    res.redirect(`/users/${user._id}/recipes`)
+   }catch(error){
+    console.log(error)
+    res.redirect('/')
+   }
+
+})
 module.exports = router
